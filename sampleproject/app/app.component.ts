69a386f04dec2089fd17ef15f018091994f636c2
solankepl <solankepl@gmail.com>
2017-06-26 11:29:11 +0530

    sample project start from scratch

---
+++ sampleproject/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html', 
+  style: `
+        h1{
+            color:#FF00FF;   
+        }    
+    `
+})
+export class AppComponent { 
+   
+}
